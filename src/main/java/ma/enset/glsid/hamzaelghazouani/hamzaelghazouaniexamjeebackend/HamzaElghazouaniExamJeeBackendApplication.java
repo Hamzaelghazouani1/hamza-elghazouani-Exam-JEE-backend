1988638de90b025ded3589b941498687dfeb4a0d
hamzaelghazouani1 <elghazouani.hamza1@gmail.com>
2025-05-19 09:12:46 +0100

    feat : Initialize Spring Boot project

---
+++ src/main/java/ma/enset/glsid/hamzaelghazouani/hamzaelghazouaniexamjeebackend/HamzaElghazouaniExamJeeBackendApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.glsid.hamzaelghazouani.hamzaelghazouaniexamjeebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamzaElghazouaniExamJeeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HamzaElghazouaniExamJeeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/enset/glsid/hamzaelghazouani/hamzaelghazouaniexamjeebackend/HamzaElghazouaniExamJeeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.glsid.hamzaelghazouani.hamzaelghazouaniexamjeebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamzaElghazouaniExamJeeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
